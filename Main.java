a3093fb221075c6e8831e081e39ed9638ecd76a5
Cristian <cristianpique@gmail.com>
2017-02-17 02:04:03 +0100

    Ejercicio 1 con clases y metodos

--- Main.java
+++ Main.java
@@ -4,32 +4,128 @@ import java.util.Scanner;
 
 class Cercle{
 
-    float radi;
+    static float radi=3;
     float variacio;
+
+    static Cercle c = new Cercle();
+    static Scanner s = new Scanner(System.in);
+
+    public static void CalculVariacioCercle(){
+
+        System.out.println("Variació del radi: ");
+        c.variacio = s.nextFloat();
+        s.nextLine();
+        if(c.radi + c.variacio > 0) {
+            c.radi += c.variacio;
+        }
+        else{
+            System.out.println("Cercle no redimensionat: el radi no pot quedar negatiu.");
+        }
+
+    }
 };
 
 class Rectangle {
 
-    float base;
-    float altura;
+    static float base=2;
+    static float altura=5;
     float variaciob;
     float variacioa;
+
+    public static void CalculVariacioRectangle(){
+
+        Rectangle r = new Rectangle();
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Variació de la base: ");
+        r.variaciob = s.nextFloat();
+        s.nextLine();
+        if(r.base+r.variaciob > 0) {
+            r.base += r.variaciob;
+        }
+        else{
+            System.out.println("La Base del Rectangle no s'ha modificat: La base no pot ser negativa. ");
+        }
+
+        System.out.println("Variació de l'altura: ");
+        r.variacioa = s.nextFloat();
+        s.nextLine();
+        if(r.altura+r.variacioa > 0) {
+            r.altura += r.variacioa;
+        }
+        else{
+            System.out.println("L'Altura del Rectangle no s'ha modificat: L'altura no pot ser negativa. ");
+        }
+    }
 };
 
 class Donut {
 
-    float re;
-    float ri;
+    static float re=3;
+    static float ri=3;
     float vre;
     float vri;
+
+    public static void CalculVariacioDonut(){
+
+        Donut d = new Donut();
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Introdueix variacio radi exterior: ");
+        d.vre=s.nextFloat();
+        s.nextLine();
+        if(d.re+d.vre>d.ri){
+            d.re +=d.vre;
+        }
+        else{
+            System.out.println("El radi exterior no s'ha modificat: El Radi Exterior no pot ser negatiu");
+        }
+
+        System.out.println("Introdueix variacio radi interior: ");
+        d.vri=s.nextFloat();
+        s.nextLine();
+        if(d.ri+d.vri<d.re) {
+            d.ri +=d.vri;
+        }
+        else{
+            System.out.println("El radi interior no s'ha modificat. El Radi Interior no pot ser negatiu");
+        }
+    }
 };
 
 class Con{
 
-    float radi;
-    float generatriu;
+    static float radi=4;
+    static float generatriu=3;
     float vr;
     float vg;
+
+    public static void CalculVariacioCon(){
+
+        Con c = new Con();
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Introdueix variacio radi: ");
+        c.vr=s.nextFloat();
+        s.nextLine();
+        if(c.radi+c.vr > 0){
+            c.radi+=c.vr;
+        }
+        else{
+            System.out.println("El radi no s'ha modificat. El Radi no pot ser negatiu");
+        }
+
+        System.out.println("Introdueix variacio generatriu: ");
+        c.vg=s.nextFloat();
+        s.nextLine();
+        if(c.generatriu+c.vg > 0){
+            c.generatriu+=c.vg;
+        }
+        else{
+            System.out.println("La generatriu no s'ha modificat. La Generatriu no pot ser negativa");
+        }
+
+    }
 };
 
 public class Main {
@@ -37,27 +133,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Cercle c = new Cercle();
-        c.radi=3;
-
-        Rectangle r = new Rectangle();
-        r.base=2;
-        r.altura=5;
-
-        Donut d = new Donut();
-        d.re=3;
-        d.ri=3;
-
-        Con co = new Con();
-        co.radi=4;
-        co.generatriu=3;
-
         String opcio;
         while(true){
-            System.out.println("Area cercle: " + 2*Math.PI*c.radi);
-            System.out.println("Area rectangle: " + r.base*r.altura);
-            System.out.println("Area donut: " + (2*Math.PI*d.re - 2*Math.PI*d.ri));
-            System.out.println("Area con : "+(Math.PI*co.radi*co.generatriu+Math.PI*Math.pow(co.radi,2)));
+            System.out.println("Area cercle: " + 2*Math.PI*Cercle.radi);
+            System.out.println("Area rectangle: " + Rectangle.base*Rectangle.altura);
+            System.out.println("Area donut: " + (2*Math.PI*Donut.re - 2*Math.PI*Donut.ri));
+            System.out.println("Area con : "+(Math.PI*Con.radi*Con.generatriu+Math.PI*Math.pow(Con.radi,2)));
 
             System.out.println("a Redimensionar cercle");
             System.out.println("b Redimensionar rectangle");
@@ -66,83 +147,17 @@ public class Main {
 
             opcio = scanner.nextLine();
             switch (opcio){
-                case "a":
-                    System.out.println("Variació del radi: ");
-                    c.variacio = scanner.nextFloat();
-                    scanner.nextLine();
-                    if(c.radi + c.variacio > 0) {
-                        c.radi += c.variacio;
-                    }else{
-                        System.out.println("Cercle no redimensionat: el radi no pot quedar negatiu.");
-                    }
-                    break;
+                case "a":Cercle.CalculVariacioCercle();
+                         break;
 
-                case "b":
-                    System.out.println("Variació de la base: ");
-                    r.variaciob = scanner.nextFloat();
-                    scanner.nextLine();
-                    if(r.base+r.variaciob > 0) {
-                        r.base += r.variaciob;
-                    }
-                    else{
-                        System.out.println("La Base del Rectangle no s'ha modificat: La base no pot ser negativa. ");
-                    }
+                case "b":Rectangle.CalculVariacioRectangle();
+                         break;
 
-                    System.out.println("Variació de l'altura: ");
-                    r.variacioa = scanner.nextFloat();
-                    scanner.nextLine();
-                    if(r.altura+r.variacioa > 0) {
-                        r.altura += r.variacioa;
-                    }
-                    else{
-                        System.out.println("L'Altura del Rectangle no s'ha modificat: L'altura no pot ser negativa. ");
-                }
-                    break;
+                case "c":Donut.CalculVariacioDonut();
+                         break;
 
-                case "c":
-                    System.out.println("Introdueix variacio radi exterior: ");
-                    d.vre=scanner.nextFloat();
-                    scanner.nextLine();
-                    if(d.re+d.vre>d.ri){
-                        d.re +=d.vre;
-                    }
-                    else{
-                        System.out.println("El radi exterior no s'ha modificat: El Radi Exterior no pot ser negatiu");
-                    }
-
-                    System.out.println("Introdueix variacio radi interior: ");
-                    d.vri=scanner.nextFloat();
-                    scanner.nextLine();
-                    if(d.ri+d.vri<d.re) {
-                        d.ri +=d.vri;
-                    }
-                    else{
-                        System.out.println("El radi interior no s'ha modificat. El Radi Interior no pot ser negatiu");
-                    }
-                    break;
-
-                case "d":
-
-                    System.out.println("Introdueix variacio radi: ");
-                    co.vr=scanner.nextFloat();
-                    scanner.nextLine();
-                    if(co.radi+co.vr > 0){
-                            co.radi+=co.vr;
-                    }
-                    else{
-                        System.out.println("El radi no s'ha modificat. El Radi no pot ser negatiu");
-                    }
-
-                    System.out.println("Introdueix variacio generatriu: ");
-                    co.vg=scanner.nextFloat();
-                    scanner.nextLine();
-                    if(co.generatriu+co.vg > 0){
-                        co.generatriu+=co.vg;
-                    }
-                    else{
-                        System.out.println("La generatriu no s'ha modificat. La Generatriu no pot ser negativa");
-                    }
-
+                case "d":Con.CalculVariacioCon();
+                         break;
             }
         }
     }
